e84524e1d5f1bda42cdb6ab797e9750090792e94
Daniel Velásquez <141380631+DannyLaine158@users.noreply.github.com>
2024-01-29 08:42:23 -0600

    Add files via upload

---
+++ Ejemplo 1/vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+export default defineConfig({
+  base: "/",
+  plugins: [react()],
+  preview: {
+    port: 5173,
+    strictPort: true,
+  },
+  server: {
+    port: 5173,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:5173",
+  },
+});
